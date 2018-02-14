4c9003b843665acd293b5d14553ff368c4f83e92
Ci Kang <35593675+e0052813@users.noreply.github.com>
2018-02-14 16:11:18 +0800

    Clean up some redundant declaring in Textui class

--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -27,19 +27,6 @@ public class TextUi {
     private static final String LINE_PREFIX = "|| ";
 
     /**
-     * A platform independent line separator.
-     */
-    private static final String LS = System.lineSeparator();
-
-    private static final String DIVIDER = "===================================================";
-
-    /**
-     * Format of indexed list item
-     */
-    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
-
-
-    /**
      * Offset required to convert between 1-indexing and 0-indexing.
      */
     public static final int DISPLAYED_INDEX_OFFSET = 1;
